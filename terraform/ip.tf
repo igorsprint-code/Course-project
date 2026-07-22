9a530c8bc9fd280fa2f26b211ca4d8177f27a430
igorsprint-code <igorsprint@gmail.com>
2026-07-22 19:51:08 +0300

    screen

--- terraform/ip.tf
+++ terraform/ip.tf
@@ -1,15 +1,10 @@
 resource "local_file" "pub_ip" {
   content  = <<-XYZ
-  [bastion]
-  ${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}
-
-  
+   
   [grafana_server]
   ${yandex_compute_instance.grafana_vm.network_interface.0.nat_ip_address}
   
  
-
-
   [kibana_server]
   ${yandex_compute_instance.kibana_vm.network_interface.0.nat_ip_address}
  
